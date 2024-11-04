992cfaec2d59f818ef9fa06fb257d2bbd0a022f7
Marti820 <j.vasquezc@uniandes.edu.co>
2024-11-04 17:25:06 -0500

    cambio en la tabla

--- ProtocoloServidor.java
+++ ProtocoloServidor.java
@@ -8,11 +8,11 @@ public class ProtocoloServidor {
     private Map<String, Integer> tablaPaquetes;
 
     public ProtocoloServidor() {
-        tablaPaquetes = new HashMap<>();
+     /*    tablaPaquetes = new HashMap<>();
         tablaPaquetes.put("user1|package1", EstadoPaquete.ENOFICINA);
         tablaPaquetes.put("user2|package2", EstadoPaquete.RECOGIDO);
         tablaPaquetes.put("user3|package3", EstadoPaquete.ENCLASIFICACION);
-        tablaPaquetes.put("user4|package4", EstadoPaquete.DESPACHADO);
+        tablaPaquetes.put("user4|package4", EstadoPaquete.DESPACHADO); */
         tablaPaquetes.put("user1|package1", EstadoPaquete.ENOFICINA);
         tablaPaquetes.put("user2|package2", EstadoPaquete.RECOGIDO);
         tablaPaquetes.put("user3|package3", EstadoPaquete.ENCLASIFICACION);
