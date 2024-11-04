d8a9813e94bae9552d11e916935ae3070825e414
Marti820 <j.vasquezc@uniandes.edu.co>
2024-11-04 12:07:59 -0500

    generador de llaves

--- Servidor.java
+++ Servidor.java
@@ -1,14 +1,73 @@
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Scanner;
 
 public class Servidor {
     public static final int PUERTO = 3400;
     private static boolean continuar = true;
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket ss = null;
+    public static void main(String[] args) throws NoSuchAlgorithmException, FileNotFoundException, IOException{
+        Scanner scanner = new Scanner(System.in);
+        int opcion = 0;
 
+        while (opcion != 2) {
+            System.out.println("===== Menú de Opciones =====");
+            System.out.println("1. Generar llaves simetricas");
+            System.out.println("2. Iniciar servidor");
+            System.out.print("Elige una opción: ");
+            
+            opcion = scanner.nextInt();
+
+            if (opcion == 1) {
+                System.out.println("GENERANDO LLAVES EN LOS ARCHIVOS: ");
+                generarLlaves();
+            } else if (opcion == 2) {
+                System.out.println("INICIANDO EL SERVIDOR");
+                try {
+                    iniciarServidor();
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            } else {
+                System.out.println("Opción inválida. Por favor, elige 1 o 2.");
+            }
+            System.out.println(); 
+        }
+        scanner.close(); 
+    }
+    public static void generarLlaves() throws NoSuchAlgorithmException, FileNotFoundException, IOException{
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+		generator.initialize(1024);
+		KeyPair keyPair = generator.generateKeyPair();
+		PublicKey publica = keyPair.getPublic();
+		PrivateKey privada = keyPair.getPrivate();
+
+        
+        try (FileOutputStream fos = new FileOutputStream("publicKey.key")) {
+            fos.write(publica.getEncoded());
+        }
+
+       
+        try (FileOutputStream fos = new FileOutputStream("privateKey.key")) {
+            fos.write(privada.getEncoded());
+        }
+
+        System.out.println("Las llaves han sido generadas y guardadas en archivos planos.");
+    }
+    
+
+
+    public static void iniciarServidor() throws IOException{
+        ServerSocket ss = null;
         System.out.println("Main Server ...");
 
         try {
@@ -27,5 +86,6 @@ public class Servidor {
             thread.start();
         }
         ss.close();
+
     }
 }
